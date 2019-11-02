7c572387567f15590071c87db3c1760ab7522c9a
Ariel Huang <ariel92911@gmail.com>
2019-11-02 22:23:40 +0800

    add imgur

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
