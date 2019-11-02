a02bc0e73a959164abbfcea9ad740c05859538fb
Ariel Huang <ariel92911@gmail.com>
2019-11-02 15:30:35 +0800

    heroku init

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const passport = require('./config/passport')
 
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
