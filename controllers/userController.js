290ab16f9cf5058f998cd502711078853dd9efbe
Ariel Huang <ariel92911@gmail.com>
2019-11-10 18:11:37 +0800

    Add user profile page

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,12 @@
+const fs = require('fs')
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../models')
 const User = db.User
 const Favorite = db.Favorite
 const Followship = db.Followship
 const Like = db.Like
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 const userController = {
   signUpPage: (req, res) => {
@@ -147,6 +150,57 @@ const userController = {
           })
       })
   },
+
+  getUser: (req, res) => {
+    User.findByPk(req.params.id)
+      .then(user => {
+        return res.render('user', { user })
+      })
+  },
+
+  editUser: (req, res) => {
+    return User.findByPk(req.params.id).then(user => {
+      return res.render('editUser', { user })
+    })
+  },
+
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : user.image
+            })
+              .then((user) => {
+                req.flash('success_messages", "profile was successfully to update')
+                res.redirect(`/users/${user.id}`)
+              })
+          })
+      })
+    } else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          })
+            .then((user) => {
+              req.flash('success_messages", "profile was successfully to update')
+              res.redirect(`/users/${user.id}`)
+            })
+        })
+    }
+  }
 }
+
 
 module.exports = userController
