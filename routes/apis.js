29dcc07c48baa899d21e1e7abd0a01981823030b
Ariel Huang <ariel92911@gmail.com>
2019-11-13 22:49:49 +0800

    install jsonwebtoken & passport-jwt and refactor POST /api/signin (return jwt)

--- routes/apis.js
+++ routes/apis.js
@@ -7,12 +7,17 @@ const upload = multer({ dest: 'temp/' })
 
 const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
+const userController = require('../controllers/api/userController.js')
 
 router.get('/admin/restaurants', adminController.getRestaurants)
 router.get("/admin/restaurants/:id", adminController.getRestaurant)
 router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
 router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
 
+
 router.get("/admin/categories", categoryController.getCategories)
+
+// JWT signin
+router.post('/signin', userController.signIn)
 
 module.exports = router
